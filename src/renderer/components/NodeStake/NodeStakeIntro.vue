9fc10f9b32440619ed8ebcd79df588e7bbf9c728
MickeyWang <1244134672@qq.com>
2018-08-13 15:56:07 +0800

    tmp

---
+++ src/renderer/components/NodeStake/NodeStakeIntro.vue
@@ -0,0 +1,37 @@
+<style scoped>
+.intro-content {
+    width:540px;
+    margin:4rem auto;
+}
+.btn-known {
+    margin:20px auto;
+    text-align: center;
+}
+.btn-known button {
+    width:150px !important;
+}
+</style>
+<template>
+    <div>
+        <div class="intro-content">
+            Ontology Triones种子节点招募规则
+            xxxxxxxxxxxxxxxxxxxx
+        </div>
+        <div class="footer-btns">
+            <div class="btn-known">
+                <a-button type="primary" class="btn-next" @click="next">{{$t('nodeStake.iHaveKnown')}}</a-button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NodeStakeIntro',
+    methods: {
+        next() {
+            this.$router.push({name: 'NodeStake'})
+        }
+    }
+}
+</script>
