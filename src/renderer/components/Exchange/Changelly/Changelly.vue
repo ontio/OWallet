7e0d8136941cc0602a2118a98ecbde66c35cf8b6
ericyang <1114341853@qq.com>
2019-03-20 14:21:02 +0800

    ref_id and merchant_id

--- src/renderer/components/Exchange/Changelly/Changelly.vue
+++ src/renderer/components/Exchange/Changelly/Changelly.vue
@@ -18,9 +18,8 @@
       <breadcrumb  :current="$t('exchange.exchange')" v-on:backEvent="handleRouteBack"></breadcrumb>
       <!-- Remove this div and enable this.loadPage in mounted() to open in a new window -->
       <div class="changelly"
-        <iframe src="https://widget.changelly.com?currencies=&from=btc&to=ont&amount=1&address=&fiat=true&fixedTo=true&theme=default&ref_id=iyaqtolcjujpxd9t" width="100%" height="600" class="changelly" scrolling="no" onLoad="function v(e){var t=e.target,n=t.parentNode,r=t.contentWindow,a=function(){return r.postMessage({width:n.offsetWidth},'https://widget.changelly.com')};window.addEventListener('resize',a),a()};v.apply(this, arguments);" style="overflow-y: hidden; border: none">Can't load widget</iframe>
-        </div>
-   </div>
+        <iframe src="https://widget.changelly.com?currencies=&from=btc&to=ont&amount=1&address=&fiat=true&fixedTo=false&theme=default&ref_id=su5srryl1mhz4fno&merchant_id=su5srryl1mhz4fno" width="100%" height="600" class="changelly" scrolling="no" onLoad="function v(e){var t=e.target,n=t.parentNode,r=t.contentWindow,a=function(){return r.postMessage({width:n.offsetWidth},'https://widget.changelly.com')};window.addEventListener('resize',a),a()};v.apply(this, arguments);" style="overflow-y: hidden; border: none">Can't load widget</iframe>
+      </div>
 </template>
 
 <script>
