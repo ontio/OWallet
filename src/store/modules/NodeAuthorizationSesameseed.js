7a760f7ab42ef682fb06298d7c0d40594df9b4fa
Daniel Blanco <dblancoparla@gmail.com>
2021-07-23 11:48:38 +0200

    Revert "Staked and Unstaked values now are fetched from sesameseed API"
    This reverts commit 99186391f3fffc8f06484bbdf05a8d0408551156.

--- src/store/modules/NodeAuthorizationSesameseed.js
+++ src/store/modules/NodeAuthorizationSesameseed.js
@@ -20,10 +20,13 @@ const mutations = {
 const actions = {
     async fetchSSPerInfo({ commit }, address) {
         try {
-            const baseUrl = 'https://api.sesameseed.org/voting/v1/address/'
+            const baseUrl = 'https://dappnode1.ont.io:10334/api/v1/storage/a63c33d2209854feafbf40685a33d4846ee82556/'
 
-            const pendingWithdrawalsPromise = axios.get(baseUrl + 'unstaked/' + address)
-            const votesPromise = axios.get(baseUrl + 'staked/' + address)
+            const hexAddress = utils.str2hexstr(address)
+            const pendingHexAddress = utils.str2hexstr('1' + address)
+
+            const pendingWithdrawalsPromise = axios.get(baseUrl + pendingHexAddress)
+            const votesPromise = axios.get(baseUrl + hexAddress)
 
             const [pendingWithdrawalsResponse, votesResponse] = await Promise.all([pendingWithdrawalsPromise, votesPromise])
 
@@ -32,12 +35,16 @@ const actions = {
 
             if (pendingWithdrawalsResponse.status === 200 && pendingWithdrawalsResponse.data) {
                 const resultPending = pendingWithdrawalsResponse.data
-                pendingWithdrawals = resultPending.value ? resultPending.value : 0
+                const pendingWithdrawalsHex = resultPending.Result ? resultPending.Result : 0
+                const reversedHex = pendingWithdrawalsHex.toString().length > 2 ? utils.reverseHex(pendingWithdrawalsHex) : pendingWithdrawalsHex
+                pendingWithdrawals = parseInt(reversedHex, 16)
             }
 
             if (votesResponse.status === 200 && votesResponse.data) {
                 const resultVotes = votesResponse.data
-                votes = resultVotes.value ? resultVotes.value : 0
+                const votesHex = resultVotes.Result ? resultVotes.Result : 0
+                const reversedHex = votesHex.toString().length > 2 ? utils.reverseHex(votesHex) : votesHex
+                votes = parseInt(reversedHex, 16)
             }
 
             commit('UPDATE_CURRENT_SS_PEER', { votes, pendingWithdrawals })
