a81750dffd5c5f4c5efd00af6257993d367a4ff1
Daniel Blanco <dblancoparla@gmail.com>
2021-07-23 11:48:53 +0200

    Revert "Sesameseed: Fixed translation from Hex Little Endian to Decimal"
    This reverts commit e44c09bf70ab2360904515041389dfd8d21f0a15.

--- src/store/modules/NodeAuthorizationSesameseed.js
+++ src/store/modules/NodeAuthorizationSesameseed.js
@@ -1,4 +1,4 @@
-import { utils } from 'ontology-ts-sdk'
+import {utils} from 'ontology-ts-sdk'
 import axios from 'axios';
 
 const state = {
@@ -18,35 +18,33 @@ const mutations = {
 }
 
 const actions = {
-    async fetchSSPerInfo({ commit }, address) {
+    async fetchSSPerInfo({commit}, address) {
         try {
             const baseUrl = 'https://dappnode1.ont.io:10334/api/v1/storage/a63c33d2209854feafbf40685a33d4846ee82556/'
-
+            
             const hexAddress = utils.str2hexstr(address)
             const pendingHexAddress = utils.str2hexstr('1' + address)
 
             const pendingWithdrawalsPromise = axios.get(baseUrl + pendingHexAddress)
             const votesPromise = axios.get(baseUrl + hexAddress)
-
-            const [pendingWithdrawalsResponse, votesResponse] = await Promise.all([pendingWithdrawalsPromise, votesPromise])
-
+    
+            const [pendingWithdrawalsResponse, votesResponse] = await Promise.all([ pendingWithdrawalsPromise, votesPromise ])
+         
             let votes = 0
             let pendingWithdrawals = 0
 
             if (pendingWithdrawalsResponse.status === 200 && pendingWithdrawalsResponse.data) {
                 const resultPending = pendingWithdrawalsResponse.data
                 const pendingWithdrawalsHex = resultPending.Result ? resultPending.Result : 0
-                const reversedHex = pendingWithdrawalsHex.toString().length > 2 ? utils.reverseHex(pendingWithdrawalsHex) : pendingWithdrawalsHex
-                pendingWithdrawals = parseInt(reversedHex, 16)
+                pendingWithdrawals = parseInt(pendingWithdrawalsHex, 16)
             }
 
             if (votesResponse.status === 200 && votesResponse.data) {
                 const resultVotes = votesResponse.data
                 const votesHex = resultVotes.Result ? resultVotes.Result : 0
-                const reversedHex = votesHex.toString().length > 2 ? utils.reverseHex(votesHex) : votesHex
-                votes = parseInt(reversedHex, 16)
+                votes = parseInt(votesHex, 16)
             }
-
+           
             commit('UPDATE_CURRENT_SS_PEER', { votes, pendingWithdrawals })
             return { votes, pendingWithdrawals };
         } catch (err) {
